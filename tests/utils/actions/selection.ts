fbc26b407331bb7d4f8bb0e3fe3d12ea7ae2a8c0
Yifeng Wang <doodlewind@toeverything.info>
2023-03-23 10:59:58 +0800

    chore: add temp test utils

--- tests/utils/actions/selection.ts
+++ tests/utils/actions/selection.ts
@@ -12,3 +12,39 @@ export async function getRichTextBoundingBox(
     return bbox;
   }, blockId);
 }
+
+async function scrollToDistance(page: Page, distance: number) {
+  await page.evaluate(distance => {
+    const viewport = document.querySelector('.affine-default-viewport');
+    if (!viewport) {
+      throw new Error();
+    }
+    viewport.scrollTo(0, distance);
+  }, distance);
+}
+
+async function getViewportAndContainer(page: Page) {
+  return await page.evaluate(() => {
+    const viewport = document.querySelector('.affine-default-viewport');
+    if (!viewport) {
+      throw new Error();
+    }
+    const container = viewport.querySelector(
+      'affine-frame .affine-block-children-container'
+    );
+    if (!container) {
+      throw new Error();
+    }
+    return { viewport, container };
+  });
+}
+
+async function getFirstAndLastElements(page: Page) {
+  const { container } = await getViewportAndContainer(page);
+  const first = container.firstElementChild;
+  const last = container.lastElementChild;
+  if (!first || !last) {
+    throw new Error();
+  }
+  return { first, last };
+}
